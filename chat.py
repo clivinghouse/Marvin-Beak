6a33c6e8c4705ab946d49c49acf87490bb59414a
chl5158 <79345446+clivinghouse@users.noreply.github.com>
2023-05-20 12:20:26 -0400

    done for now

--- chat.py
+++ chat.py
@@ -1,11 +1,14 @@
 
 import openai
-class chat: openai.api_key = "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"
+class chat: openai.api_key = "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"
 
 models = openai.Model.list()
-  def resp(msg):
-     chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": msg}])
-     return chat_completion
+def resp(msg):
+  chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": msg}])
+  chatString = ""
+  for choices in chat_completion.choices:
+    chatString += choices.message.content;
+  return chatString
 
 # print the chat completion
 #print(chat_completion.choices[0].message.content)
